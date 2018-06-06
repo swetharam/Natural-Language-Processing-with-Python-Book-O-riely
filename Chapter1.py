ff1af06b6abc3be9ddad7f165db614fb445d2251
Swetha Ramaswamy <swetharamaswamy66@gmail.com>
2018-06-05 19:24:42 -0500

    Initial Commit

---
+++ Chapter1.py
@@ -0,0 +1,3 @@
+import nltk                  #importing the NLTK module in the program
+from nltk.book import *      #import all the data from the nltk.book module
+print(text1)                 # printing the text 1 ; essentially the header and the description of text 1
